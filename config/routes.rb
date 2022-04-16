7fc71dc02822d043c41b1d713ba1963b2c73625c
Etienne de Dianous <etiennededi@hotmail.fr>
2022-04-16 15:58:12 +0200

    Working, need styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+  get 'articles/index', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'article/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
